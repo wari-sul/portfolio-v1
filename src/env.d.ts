74e2147f3c51550c46bbd6c3ab606eecd22f95c6
wari-sul <official.warisul@gmail.com>
2026-05-19 01:56:45 +0600

    feat: implement StaticForms contact form with honeypot spam protection

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_STATICFORMS_KEY: string;
+  readonly GIST_STATS_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
