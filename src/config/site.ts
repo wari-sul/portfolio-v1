6eaa9b7829012ad688e7341376b1269b776baa74
wari-sul <official.warisul@gmail.com>
2026-06-01 04:44:29 +0600

    chore: save pre-implementation changes for portfolio V3

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,12 +1,13 @@
 export const siteConfig = {
   name: "SM WARISUL A. RAFIN",
   title: "SM WARISUL A. RAFIN - Portfolio | SysOps, Cloud, DevOps, Web Dev",
-  description: "I'm a versatile IT professional passionate about building robust and scalable infrastructure. Expertise in systems administration, virtualization, and containerization.",
+  description: "I'm a versatile IT professional passionate about building robust and scalable infrastructure. My expertise lies in systems administration, virtualization, and containerization, leveraging technologies like Proxmox, Docker, and Kubernetes. I excel at automating and optimizing infrastructure and thrive in challenging roles, particularly in DevOps or SRE. I'm a continuous learner, always expanding my skillset with the latest technologies.",
   email: "contact@warisul.com",
   links: {
     github: "https://github.com/wari-sul",
     linkedin: "https://www.linkedin.com/in/warisul-rafin",
     dashboard: "https://dashboard.warisul.com/",
+    hashnode: "https://hashnode.com/warisul",
   },
   stats: {
     projects: "9+",
